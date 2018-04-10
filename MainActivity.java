a9ed5e754145c94faea72f2b344c6315032a266e
Sachi Verma <35468660+sachiverma@users.noreply.github.com>
2018-04-10 16:27:41 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,245 +1,230 @@
-package com.example.theodhor.speechapplication;
+package com.example.sachiv.menu2;
 
-import android.content.ActivityNotFoundException;
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Parcelable;
-import android.provider.AlarmClock;
-import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.UtteranceProgressListener;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.util.Log;
+
+import android.support.v7.widget.CardView;
+
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ListView;
+
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import android.widget.GridView;
+
 import android.widget.Toast;
 
-import com.DataSample;
-
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity{
-
-
-        private ListView listView;
-        private com.javapapers.android.csvfileread.app.ItemArrayAdapter itemArrayAdapter;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            listView = (ListView) findViewById(R.id.listView);
-            itemArrayAdapter = new com.javapapers.android.csvfileread.app.ItemArrayAdapter(getApplicationContext(), R.layout.item_layout);
-
-            Parcelable state = listView.onSaveInstanceState();
-            listView.setAdapter(itemArrayAdapter);
-            listView.onRestoreInstanceState(state);
-
-            InputStream inputStream = getResources().openRawResource(R.raw.stats);
-            CSVFile csvFile = new CSVFile(inputStream);
-            List scoreList = csvFile.read();
-
-            for(String[] scoreData:scoreList ) {
-                itemArrayAdapter.add(scoreData);
-            }
-        }
-    private List<DataSample> dataSamples= new ArrayList<>();
-    private void readCSVData() throws IOException {
-        DataSample sample;
-        InputStream is = getResources().openRawResource(R.raw.diseaseresults);
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(is, Charset.forName("UTP-8"))
-        );
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                String[] tokens = line.split(",");
-                DataSample sample = new DataSample();
-                sample.setDisease(tokens[0]);
-                sample.setGenes_tested(tokens[1]);
-                sample.setResult(tokens[2]);
-                sample.setTag(tokens[3]);
-                dataSamples.add(sample);
-                Log.d("MyActivity", "Just created: " + sample);
-            }
-        }
-
-    }
-
-    private TextToSpeech tts;
-    private ArrayList<String> questions;
-    private String name, surname, age, asName;
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
-    private static final String PREFS = "prefs";
-    private static final String NEW = "new";
-    private static final String NAME = "name";
-    private static final String AGE = "age";
-    private static final String AS_NAME = "as_name";
+public class MainActivity extends AppCompatActivity {
+    GridLayout mainGrid;
+    Button button;
+    Button download_button;
+    private static final int MY_PERMISSION = 1;
+    ProgressDialog mProgressDialog;
+    double file_size = 0;
+    String file_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        preferences = getSharedPreferences(PREFS,0);
-        editor = preferences.edit();
+        button = (Button) findViewById(R.id.reportbutton);
+        download_button = (Button) findViewById(R.id.downloadbutton);
 
-        findViewById(R.id.microphoneButton).setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                listen();
+                reportpage();
             }
         });
-        loadQuestions();
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
 
-        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+        download_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    int result = tts.setLanguage(Locale.US);
-                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Log.e("TTS", "This Language is not supported");
-                    }
-                    speak("Hello");
-
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION);
                 } else {
-                    Log.e("TTS", "Initilization Failed!");
+                    File dir =  new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                            + "/MyDownloadedFiles/" + file_name);
+                    try {
+                        dir.mkdir();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this,"Cannot create folder!",Toast.LENGTH_SHORT).show();
+                    }
+                    //file location
+                    new DownloadTask().execute("http://www.wingnity.com/files/Hall_of_the_Mountain_King.mp3");
+
                 }
             }
         });
-
-    }
-
-    private void loadQuestions(){
-        questions = new ArrayList<>();
-        questions.clear();
-        questions.add("Hello, what is your name?");
-        questions.add("What is your surname?");
-        questions.add("How old are you?");
-        questions.add("That's all I had, thank you ");
-    }
-
-    private void listen(){
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
-
-        try {
-            startActivityForResult(i, 100);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(MainActivity.this, "Your device doesn't support Speech Recognition", Toast.LENGTH_SHORT).show();
-        }
     }
 
     @Override
-    public void onDestroy() {
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
-        super.onDestroy();
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private void speak(String text){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-
-        }else{
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-        }
+    public void reportpage() {
+        Intent startNewActivity = new Intent(this, report.class);
+        startActivity(startNewActivity);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100){
-            if (resultCode == RESULT_OK && null != data) {
-                ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                String inSpeech = res.get(0);
-                recognition(inSpeech);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    private class DownloadTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            file_name = strings[0].substring(strings[0].lastIndexOf("/") + 1);
+            try {
+                InputStream input = null;
+                OutputStream output = null;
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL(strings[0]);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.connect();
+                    if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                        return "Server returned HTTP" + connection.getResponseCode() + " " +
+                                connection.getResponseMessage();
+                    }
+                    int fileLength = connection.getContentLength();
+                    file_size = fileLength;
+
+                    input = connection.getInputStream();
+                    output = new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath()
+                            + "/MyDownloadedFiles/" + file_name);
+
+                    byte data[] = new byte[4096];
+                    long total = 0;
+                    int count;
+                    while ((count = input.read(data)) != -1) {
+                        if (isCancelled()) {
+                            return null;
+                        }
+                        total += count;
+                        if (fileLength > 0) {
+                            publishProgress((int) (total * 100 / fileLength));
+                        }
+                        output.write(data, 0, count);
+                    }
+                } catch (Exception e) {
+                    return e.toString();
+                } finally {
+                    try {
+                        if (output != null) {
+                            output.close();
+                        }
+                        if (input != null) {
+                            input.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            } finally {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("downloading...");
+            mProgressDialog.setMessage("File size: 0 MB");
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            mProgressDialog.setCancelable(true);
+
+            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    Toast.makeText(MainActivity.this, "Download cancelled", Toast.LENGTH_SHORT).show();
+                    File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                            + "/MyDownloadedFiles/" + file_name);
+
+                    try {
+                        dir.delete();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(values[0]);
+            mProgressDialog.setMessage("File size: " + new DecimalFormat("##.##").format(file_size / 1000000) + " MB");
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mProgressDialog.dismiss();
+            if (result != null) {
+                Toast.makeText(MainActivity.this, "Error: " + result, Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(MainActivity.this, "Downloaded!" + result, Toast.LENGTH_SHORT).show();
+
             }
         }
     }
-
-    private void recognition(String text){
-        Log.e("Speech",""+text);
-        String[] speech = text.split(" ");
-        if(text.contains("hello")){
-            speak(questions.get(0));
-        }
-        //
-        if(text.contains("my name is")){
-            name = speech[speech.length-1];
-            Log.e("THIS", "" + name);
-            editor.putString(NAME,name).apply();
-            speak(questions.get(2));
-        }
-        //This must be the age
-        if(text.contains("years") && text.contains("old")){
-            String age = speech[speech.length-3];
-            Log.e("THIS", "" + age);
-            editor.putString(AGE, age).apply();
-        }
-
-        if(text.contains("what time is it")){
-            SimpleDateFormat sdfDate = new SimpleDateFormat("HH:mm");//dd/MM/yyyy
-            Date now = new Date();
-            String[] strDate = sdfDate.format(now).split(":");
-            if(strDate[1].contains("00"))
-                strDate[1] = "o'clock";
-            speak("The time is " + sdfDate.format(now));
-
-        }
-
-        if(text.contains("wake me up at")){
-            speak(speech[speech.length-1]);
-            String[] time = speech[speech.length-1].split(":");
-            String hour = time[0];
-            String minutes = time[1];
-            Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
-            i.putExtra(AlarmClock.EXTRA_HOUR, Integer.valueOf(hour));
-            i.putExtra(AlarmClock.EXTRA_MINUTES, Integer.valueOf(minutes));
-            startActivity(i);
-            speak("Setting alarm to ring at " + hour + ":" + minutes);
-        }
-
-        if(text.contains("thank you")){
-            speak("Thank you too " + preferences.getString(NAME, null));
-        }
-
-        if(text.contains("how old am I")){
-            speak("You are "+preferences.getString(AGE,null)+" years old.");
-        }
-
-        if(text.contains("what is your name")){
-            String as_name = preferences.getString(AS_NAME,"");
-            if(as_name.equals(""))
-                speak("How do you want to call me?");
-            else
-                speak("My name is "+as_name);
-        }
-
-        if(text.contains("call you")){
-            String name = speech[speech.length-1];
-            editor.putString(AS_NAME,name).apply();
-            speak("I like it, thank you "+preferences.getString(NAME,null));
-        }
-
-        if(text.contains("what is my name")){
-            speak("Your name is "+preferences.getString(NAME,null));
-        }
-    }
 }
+
+
+
+
+
+
+
+
+
